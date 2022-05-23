d7dba10292ba148b0014b61ec239f671a2b648fe
nastjamakh <nastja.makhonina.dev@gmail.com>
2022-05-23 20:37:10 +0200

    run app detached

--- bin/web-app-start.sh
+++ bin/web-app-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # download latest model from S3
-docker exec -i home-credit-risk poetry run uvicorn api:app --host 0.0.0.0
+docker exec -d -i home-credit-risk poetry run uvicorn api:app --host 0.0.0.0
