01a9a262c32c1377ec57b4119d5154c431516ecb
nastjamakh <nastja.makhonina.dev@gmail.com>
2022-05-23 19:49:41 +0200

    update script path

--- bin/init.sh
+++ bin/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # download latest model from S3
-~/home-credit-risk/bin/docker-exec.sh poetry run model load_from_s3
+/home/ubuntu/home-credit-risk/bin/docker-exec.sh poetry run model load_from_s3
