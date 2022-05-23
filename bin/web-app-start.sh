f0eeb1302dda599d4cefc0e35c615a5ddb8b490b
nastjamakh <nastja.makhonina.dev@gmail.com>
2022-05-23 20:32:13 +0200

    remove env variables from web app start script

--- bin/web-app-start.sh
+++ bin/web-app-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # download latest model from S3
-API_KEY=foo  PYTHONPATH=${PWD}/src docker exec -i home-credit-risk poetry run uvicorn api:app --reload --port 8012 --host 0.0.0.0
+docker exec -i home-credit-risk poetry run uvicorn api:app --host 0.0.0.0
