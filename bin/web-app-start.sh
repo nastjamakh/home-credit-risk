3a794fe31b1a568e63f2958024c4cec2f90fd028
nastjamakh <nastja.makhonina.dev@gmail.com>
2022-05-23 20:53:56 +0200

    add api key as env variable

--- bin/web-app-start.sh
+++ bin/web-app-start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # download latest model from S3
+docker exec -i home-credit-risk /bin/bash -c "export API_KEY=foo"
 docker exec -d -i home-credit-risk poetry run uvicorn api:app --host 0.0.0.0
