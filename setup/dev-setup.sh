44603012ced4b68597dcc87fd0bbed40ffbe597f
nastjamakh <nastja.makhonina.dev@gmail.com>
2022-05-31 22:09:21 +0200

    add startup script

---
+++ setup/dev-setup.sh
@@ -0,0 +1,19 @@
+#bin/bash.sh
+
+# install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install essentials
+brew install git
+
+# install AWS CLI
+# from https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+sudo installer -pkg AWSCLIV2.pkg -target / 
+
+
+# install docker
+
+
+# Configure Docker account
+# Configure git SSH
